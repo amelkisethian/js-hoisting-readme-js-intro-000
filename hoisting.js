bbc1de3a036872529af24c28020524e2e5d742c8
amelkisethian <angela.melkisethian@gmail.com>
2017-10-28 21:01:00 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -20,14 +20,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-sayMy();
+
   function sayMy() {
   var name = "Kristin";
   console.log(name)
   }
 
-
-
-  }
 
 }
